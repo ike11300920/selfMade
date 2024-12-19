9bb48ec9f68c2bc6f0ba81c122d5b80221968937
i.k <karua0920.ike1130@gmail.com>
2024-12-19 10:17:39 +0900

    2024-12-19-interests

--- app/database/migrations/2024_11_27_044341_create_interests_table.php
+++ app/database/migrations/2024_11_27_044341_create_interests_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('interest', function (Blueprint $table) {
+        Schema::create('interests', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->integer('forum_id');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('interest');
+        Schema::dropIfExists('interests');
     }
 };
---
+++ app/database/seeders/usersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class usersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'test20',
+            'email' => 'test20@test.co.jp',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
